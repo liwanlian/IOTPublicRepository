67928d6b5ec48914c2339fad8ce7cbf26c53d2ae
liwanlian <3290098379@qq.com>
2021-06-22 18:36:26 +0800

    mqtt库增加topic

---
+++ iotboxaar/src/main/java/com/srthink/iotboxaar/models/MsgReplyInfo.java
@@ -0,0 +1,48 @@
+package com.srthink.iotboxaar.models;
+
+/**
+ * @author liwanlian
+ * @date 2021/6/22 17:56
+ */
+public class MsgReplyInfo {
+
+    /**
+     * body : {"printerNum":"12134","deviceCode":"示例数据"}
+     * deviceCode : C1N7HA-MVINPT
+     * identifier : printclose
+     * messageId : 1183925259431051264
+     * productCode : R0U4QKGX2T6919222
+     * timestamp : 1624334889118
+     */
+
+    public BodyBean body;
+    public String deviceCode;
+    public String identifier;
+    public String messageId;
+    public String productCode;
+    public Long timestamp;
+
+    public MsgReplyInfo(BodyBean body, String deviceCode, String identifier, String messageId, String productCode, Long timestamp) {
+        this.body = body;
+        this.deviceCode = deviceCode;
+        this.identifier = identifier;
+        this.messageId = messageId;
+        this.productCode = productCode;
+        this.timestamp = timestamp;
+    }
+
+    public static class BodyBean {
+        /**
+         * printerNum : 12134
+         * deviceCode : 示例数据
+         */
+
+        public String printerNum;
+        public String deviceCode;
+
+        public BodyBean(String printerNum, String deviceCode) {
+            this.printerNum = printerNum;
+            this.deviceCode = deviceCode;
+        }
+    }
+}
---
+++ iotboxaar/src/main/java/com/srthink/iotboxaar/utils/mqtt/TopicUtils.java
@@ -0,0 +1,15 @@
+package com.srthink.iotboxaar.utils.mqtt;
+
+/**
+ * @author liwanlian
+ * @date 2021/6/22 18:13
+ */
+public class TopicUtils {
+    public static String topic_prop = "topic/iot/sys/%s/%s/prop";
+    //    <!--    订阅的主题  更新通知的topic-->
+    public static String topic_update = "topic/iot/sys/%s/%s/upgrade/down";
+    //    <!--    更新情况反馈的topic-->
+    public static String topic_notice = "topic/iot/sys/%s/%s/upgrade/reply";
+    //    服务调用返回结果上报
+    public static String topic_msgreply = "topic/iot/sys/%s/%s/message/reply";
+}
